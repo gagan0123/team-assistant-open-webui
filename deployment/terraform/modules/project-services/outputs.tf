b155e4d1a7ef332d4be31d9b6164e9dda100e0bb
Riddhesh Sanghvi <riddhesh237@gmail.com>
2025-07-15 18:10:42 +0530

    Remove old terraform code
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- deployment/terraform/modules/project-services/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "enabled_services" {
-  description = "List of enabled Google Cloud services"
-  value       = [for service in google_project_service.required_apis : service.service]
-}
-
-output "api_propagation_complete" {
-  description = "Indicates when API propagation is complete"
-  value       = time_sleep.api_propagation.id
-} 
